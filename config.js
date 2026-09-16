298d9f8a5617b546506715781873e3ce8f04d2de
likhith <123664920+likhithlikki@users.noreply.github.com>
2026-09-16 20:05:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /* Backend configuration. Deploy Code.gs as an Apps Script Web App and paste the /exec URL below. */
 window.APP_CONFIG={
-  APPS_SCRIPT_URL:'https://script.google.com/macros/s/AKfycbwgEX3fyl9IO-HT8FOmFL8exT8LnSaIGfnFl7L7mm31zPqhl7LdzYIfnXedzAmpo2NiCfQ/exec',
+  APPS_SCRIPT_URL:'https://script.google.com/macros/s/AKfycbyEanSa0-5YevPxB2xI3IeDXOxwzAmXA41RUgErbvlZ-hYLGZCRm5PAXtr891X5v_L69Q/exec',
   SITE_URL:window.location.origin+window.location.pathname
 };
